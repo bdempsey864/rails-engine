b4d37ad0abaaa15877012389251e6329a8d38fbe
Blake Dempsey <bdempsey864@gmail.com>
2022-03-31 16:06:57 -0400

    Update model tests for merchant/item

--- spec/model/item_spec.rb
+++ spec/model/item_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe Merchant, type: :model do
-  it 'searches by name' do
+RSpec.describe Item, type: :model do
+  it 'Item search by name' do
     merchant1 = Merchant.create(name: 'Murph')
     items = create_list(:item, 3, merchant_id: merchant1.id)
 
-    expect(Merchant.search_name('Murph')).to be_a Merchant
+    expect(Item.search_name(items.first.name).count).to eq(1)
   end
 end
